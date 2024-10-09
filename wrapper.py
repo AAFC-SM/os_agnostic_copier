cc0458ba0f044e4d24de4beb5179de7630333167
Saifullah Shaikh <saifullah.shaikh@agr.gc.ca>
2024-10-09 11:53:11 -0400

    replaced subprocess with snakemake.shell for better integration

--- wrapper.py
+++ wrapper.py
@@ -3,15 +3,24 @@ import platform
 import subprocess
 import yaml
 from snakemake.script import snakemake
+from snakemake.shell import shell
+
 # Load config file path
 
-def linux_copy():
+def linux_copy(source, base, options, required_dependencies, log = '/LOG'):
     # TO-DO -> check for rsync version
+    rsync_cmd = [
+        'rsync',
+        *options,
+        source,
+        base,
+        log
+    ]
 
-    palce ='place'
+def windows_copy(source, base, options, required_dependencies, log = '/LOG+:logs/copy.log'):
 
-def windows_copy(source, base, options, required_dependencies, log = '/LOG+:logs/robocopy.log'):
-    place = 'place'
+    options
+
     robocopy_cmd = [
         'robocopy',
         source,
@@ -22,14 +31,12 @@ def windows_copy(source, base, options, required_dependencies, log = '/LOG+:logs
         *required_dependencies
 
     ]
+    robocopy_cmd = f'robocopy {source} {base} {options} {log} {required_dependencies}'
 
     print(f"Copying required items from {source} to {base} using robocopy...")
     try:
-        result = subprocess.run(robocopy_cmd, capture_output=True, text=True)
-        if result.returncode <= 7:
-            print("Successfully copied required items.")
-        else:
-            print(f"Robocopy reported errors: {result.stdout}")
+        shell(robocopy_cmd)
+        print("Successfully copied required items.")
     except Exception as e:
         print(f"Exception during robocopy execution: {e}")
     
@@ -44,12 +51,17 @@ def windows_copy(source, base, options, required_dependencies, log = '/LOG+:logs
 
 def main():
     config_file = snakemake.params.get('config_path')
-    # Extract source and destination
+
+    # Extract from config
     source = snakemake.config['network_source']
     options = snakemake.config['options', []]
     base_dir = snakemake.config['base_dir']
     required_dirs = snakemake.config['required_dirs', []]
     required_files = snakemake.config['required_files', []]
+
+    # Convert to single string as snakemake.shell requires
+    options = " ".join(options)
+    required_files = " ".join(required_files)
 
     # Check for os
     my_platform = platform.system()
@@ -58,7 +70,7 @@ def main():
     # Check if rsync package is installed
         test = 'test'
     elif my_platform == 'Windows':
-        
+
         windows_copy(source, base_dir, options, required_files)
 
         for dir in required_dirs:
